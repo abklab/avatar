1c3df35509174daf67aaebb848cd77df9485653f
ABDUL-BASIT <qalmebasit@live.com>
2019-10-29 08:48:54 -0400

    Add project files.

---
+++ NEWHIRES/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NEWHIRES.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
